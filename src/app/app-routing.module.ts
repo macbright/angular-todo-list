0cdb9ad34348604b0ff41258b7ef4a112c56522b
bright okike <brightokike@gmail.com>
2019-10-25 18:13:55 +0300

    completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { TodoComponent } from "./components/todo/todo.component";
+import { AboutComponent } from "./components/pages/about/about.component";
 
-const routes: Routes = [{ path: "", component: TodoComponent }];
+const routes: Routes = [
+  { path: "", component: TodoComponent },
+  { path: "about", component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
